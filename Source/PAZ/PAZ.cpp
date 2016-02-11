0eea0d066aefea1f9314e15044ec5c9ade53c1f4
acaskanette <apcaskanette@gmail.com>
2016-02-11 17:54:16 -0500

    Intial project setup, basic assets

---
+++ Source/PAZ/PAZ.cpp
@@ -0,0 +1,7 @@
+// Copyright 1998-2015 Epic Games, Inc. All Rights Reserved.
+
+#include "PAZ.h"
+
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, PAZ, "PAZ" );
+ 
---
+++ Source/PAZ/PAZ.h
@@ -0,0 +1,8 @@
+// Copyright 1998-2015 Epic Games, Inc. All Rights Reserved.
+
+#ifndef __PAZ_H__
+#define __PAZ_H__
+
+#include "EngineMinimal.h"
+
+#endif
